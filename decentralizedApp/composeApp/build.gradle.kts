6a0300fdca91a4f000c2cbebf45ce398c4be0caa
nguyenphuc22 <59957741+nguyenphuc22@users.noreply.github.com>
2025-01-28 11:33:53 +0700

    Config build gradle

--- decentralizedApp/composeApp/build.gradle.kts
+++ decentralizedApp/composeApp/build.gradle.kts
@@ -16,7 +16,7 @@ kotlin {
             jvmTarget.set(JvmTarget.JVM_11)
         }
     }
-    
+
     listOf(
         iosX64(),
         iosArm64(),
@@ -27,12 +27,12 @@ kotlin {
             isStatic = true
         }
     }
-    
+
     jvm("desktop")
-    
+
     sourceSets {
         val desktopMain by getting
-        
+
         androidMain.dependencies {
             implementation(compose.preview)
             implementation(libs.androidx.activity.compose)
